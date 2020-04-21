8787c114f0a76ccc1d0eb2d167b91aea5c65bbc1
Ken Voskuil <mail@kaesve.nl>
2020-04-21 02:36:18 +0200

    Little bit of cleanup

--- simple.py
+++ simple.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
 import numpy as np
-
-from PIL import Image
 import matplotlib.pyplot as plt
 
-from tqdm import tqdm
 
 keras = tf.keras
 
+
+## Geometry
+
+def circle_sdf(v, radius = 1.):
+  return np.sqrt((v*v).sum(axis=1)) - radius
+
+
+## Model
 
 model = keras.models.Sequential([
   keras.layers.Dense(512, activation="relu"),
@@ -17,8 +22,8 @@ model = keras.models.Sequential([
 ])
 model.compile(optimizer=keras.optimizers.Adam(), loss='mse')
 
-def circle_sdf(v, radius = 1.):
-  return np.sqrt((v*v).sum(axis=1)) - radius
+
+## Training
 
 train_count = 10000
 
@@ -27,13 +32,15 @@ train_y = circle_sdf(train_X)
 
 model.fit(train_X, train_y, batch_size=32, epochs=5)
 
-test_side = 100
+
+## Evaluation
+
+test_side = 100 # generate test points in an n x n grid, so we can show the result as an image
 
 test_X = np.array([ [ x, y ] for x in range(test_side) for y in range(test_side) ])
 test_X = 5*(test_X/100 - 0.5)
 
 test_y = circle_sdf(test_X)
-
 
 predicted = model.predict(test_X)
 
