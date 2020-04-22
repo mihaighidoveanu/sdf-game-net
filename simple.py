e0e32539cf561d198da6c2c463e59f784e4b3906
Ken Voskuil <mail@kaesve.nl>
2020-04-22 22:35:24 +0200

    Add isoline rendering. Closes #5

--- simple.py
+++ simple.py
@@ -8,9 +8,17 @@ keras = tf.keras
 
 ## Geometry
 
+# these SDF functions expect arrays of points, not individual points
+
 def circle_sdf(v, radius = 1.):
   return np.sqrt((v*v).sum(axis=1)) - radius
 
+# "fast" because this is not an exact SDF, especially near the corners
+def fast_box_sdf(v, dim = np.array([ 1, 1 ])):
+  return np.max(abs(v) - dim, axis=1)
+
+
+target_sdf = fast_box_sdf
 
 ## Model
 
@@ -28,9 +36,9 @@ model.compile(optimizer=keras.optimizers.Adam(), loss='mse')
 train_count = 10000
 
 train_X = 3*np.random.normal(size=(train_count, 2))
-train_y = circle_sdf(train_X)
+train_y = target_sdf(train_X)
 
-model.fit(train_X, train_y, batch_size=32, epochs=5)
+model.fit(train_X, train_y, batch_size=32, epochs=10)
 
 
 ## Evaluation
@@ -40,13 +48,17 @@ test_side = 100 # generate test points in an n x n grid, so we can show the resu
 test_X = np.array([ [ x, y ] for x in range(test_side) for y in range(test_side) ])
 test_X = 5*(test_X/100 - 0.5)
 
-test_y = circle_sdf(test_X)
+test_y = target_sdf(test_X)
 
 predicted = model.predict(test_X)
 
 print("mean error")
 print(np.mean(predicted - test_y))
 
-plt.imshow(predicted.reshape((test_side, test_side)), 
-  cmap='hot', interpolation='nearest')
+
+
+X, Y = np.meshgrid(range(test_side), range(test_side))
+cp = plt.contourf(X, Y, predicted.reshape((test_side, test_side)))
+plt.colorbar(cp)
+plt.contour(X, Y, test_y.reshape((test_side, test_side)), colors='#ffffff99', linewidths=0.75)
 plt.show()
