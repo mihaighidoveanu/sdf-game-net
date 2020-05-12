afccd7f62b1c49af777f0af00de1fe2b42429d4b
mihai.ghidoveanu <mihai.ghidoveanu@gmail.com>
2020-05-12 11:39:29 +0200

    fix binary model script

--- src/simple-binary-shape-model.py
+++ src/simple-binary-shape-model.py
@@ -46,25 +46,19 @@ if __name__ == '__main__':
 
     ## Evaluation
 
-train_count = 10000
+    test_side = 100
 
-train_X = 3*np.random.normal(size=(train_count, 2))
-train_y = circle_sdf(train_X)
+    test_X = np.array([ [ x, y ] for x in range(test_side) for y in range(test_side) ])
+    test_X = 5*(test_X/100 - 0.5)
 
-model.fit(train_X, train_y, batch_size=32, epochs=5)
-
-test_side = 100
-
-test_X = np.array([ [ x, y ] for x in range(test_side) for y in range(test_side) ])
-test_X = 5*(test_X/100 - 0.5)
-
-test_y = circle_sdf(test_X)
+    test_y = target_sdf(test_X) < 0
 
 
-predicted = model.predict(test_X)
+    predicted = model.predict(test_X)
 
-print("mean error")
-print(np.mean(predicted - test_y))
+    print("mean error")
+    print(np.mean(predicted - test_y))
 
-plt.imshow(predicted.reshape((test_side, test_side)), 
-  cmap='hot', interpolation='nearest')
+    plt.imshow(predicted.reshape((test_side, test_side)), 
+      cmap='hot', interpolation='nearest')
+    plt.show()
