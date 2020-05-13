ec33768dce4213a15a9e0842a7cb99a1de7b45b2
Ken Voskuil <mail@kaesve.nl>
2020-05-13 17:02:05 +0200

    TSNE plots of embeddings, move more sharable code to utils, clean up and document embedding experiment

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,37 +1,26 @@
+import os
+
 from tensorflow import keras
 import tensorflow as tf
 from sklearn.manifold import TSNE
 import numpy as np
 import matplotlib.pyplot as plt
 
-def getdata(input_dim):
-    point_count = 100
-    X = np.empty((point_count, 2 + 1))
-    X[:,:2] = 0.5*np.random.normal(size=(point_count, 2))
-    char_dist = np.random.choice(input_dim, size=point_count, replace=True)
-    X[:, 2] = char_dist
-    return X, char_dist
+def project(out_dir, embedding_layer, ids, labels, name="tsne.png"):
+    os.makedirs(out_dir, exist_ok=True)
+    model = keras.Sequential([ embedding_layer ])
+    shape_embeddings = model.predict(ids).reshape((len(ids), -1))
 
-def getmodel(model):
-    embedding = model.get_layer('embedding')
-    input_dim = embedding.input_dim
-    embedding = embedding.output
-    transformer = keras.models.Model(inputs = model.input, outputs = embedding)
-    return transformer, input_dim
-
-
-def visualise(model, out_dir):
-    model, input_dim = getmodel(model)
-    x, y = getdata(input_dim)
-    embeddings = model.predict(x)
     compressor = TSNE(metric = 'euclidean')
-    e = compressor.fit_transform(embeddings)
-    plt.scatter(e[:, 0], e[:, 1], c = y, cmap = 'Reds')
+    tsne_embeddings = compressor.fit_transform(shape_embeddings)
+
+    plt.scatter(tsne_embeddings[:, 0], tsne_embeddings[:, 1])
+    for label, coord in zip(labels, tsne_embeddings):
+        plt.annotate(label, coord)
+
     plt.savefig(f'{out_dir}/tsne.png')
     plt.show()
-
-def save(model):
-    pass
+    plt.close()
 
 if __name__ == '__main__':
     pass
